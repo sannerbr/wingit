bd1f5d9e134ada432e8542d95ff45dbc057ec293
Austin Coburn <austincoburn41@gmail.com>
2020-11-10 13:48:26 -0600

    Added manufacturer class

--- server/src/main/java/learn/wingit/models/Manufacturer.java
+++ server/src/main/java/learn/wingit/models/Manufacturer.java
@@ -1,6 +1,22 @@
 package learn.wingit.models;
 
 public class Manufacturer {
+    private int manufacturer_id;
+    private String name;
 
+    public int getManufacturer_id() {
+        return manufacturer_id;
+    }
 
+    public void setManufacturer_id(int manufacturer_id) {
+        this.manufacturer_id = manufacturer_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
