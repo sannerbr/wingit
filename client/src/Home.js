ab55efc974b8de257b62118af4fafec4f5fdb23b
Wedell3 <wedel024@umn.edu>
2020-11-11 12:53:04 -0600

    added cards for each category on home page

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import {Link} from 'react-router-dom';
 
 
-class Home extends React.Component {
-    render() {
-        return (
-            <p1>Home</p1>
-        )
-    }
+
+export default function Home() {
+  return (
+    <>
+      <div className="container pt-lg-5">
+        <div className="row justify-content-md-center" >
+          <div className="col-sm-4">
+            <div className="card">
+              <div className="card-body">
+                <p className="card-text">We supply the most commercial planes to anyone. Period.</p>
+                <Link to="/commercial-planes" className="btn btn-primary">Commercial Planes</Link>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-4">
+            <div className="card">
+              <div className="card-body">
+                <p className="card-text">Our cargo planes are top tier. Fully certifiably awesome</p>
+                <Link to="/cargo-planes" className="btn btn-primary">Cargo Planes</Link>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-4">
+            <div className="card">
+              <div className="card-body">
+                <p className="card-text">The private jets we offer are the best on the market. Click here if you're rich</p>
+                <Link to="/private-planes" className="btn btn-primary">Private Planes</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
-
-export default Home;
--- client/src/MenuBar.js
+++ client/src/MenuBar.js
@@ -5,43 +5,37 @@ import {
   Link
 } from 'react-router-dom';
 
-class MenuBar extends React.Component {
-  render() {
-    return (
-      <>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className="collapse navbar-collapse">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item mr-2">
-                <Link to="/" className="btn">Home</Link>
-              </li>
-              <li className="nav-item dropdown">
-                <button className="btn dropdown-toggle" type="button" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Planes
-                </button>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <Link to="/commercial-planes" className="dropdown-item">Commercial</Link>
-                  <Link to="/cargo-planes" className="dropdown-item">Cargo</Link>
-                  <Link to="/private-planes" className="dropdown-item">Private</Link>
-                </div>
-              </li>
-              <li className="nav-item mr-2">
-                <Link to="/add-plane" className="btn">Add</Link>
-              </li>
-            </ul>
-            <ul className="navbar-nav ml-auto">
-                <li className="nav-item mr-2">
-                  <Link to="/account" className="btn">Account</Link>
-                </li>
-                <li className="nav-item mr-2">
-                  <Link to="/orders" className="btn">Orders</Link>
-                </li>
-              </ul>
-          </div>
-        </nav>
-      </>
-    )
-  }
+export default function MenuBar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="collapse navbar-collapse">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item mr-2">
+            <Link to="/" className="btn">Home</Link>
+          </li>
+          <li className="nav-item dropdown">
+            <button className="btn dropdown-toggle" type="button" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Planes
+              </button>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <Link to="/commercial-planes" className="dropdown-item">Commercial</Link>
+              <Link to="/cargo-planes" className="dropdown-item">Cargo</Link>
+              <Link to="/private-planes" className="dropdown-item">Private</Link>
+            </div>
+          </li>
+          <li className="nav-item mr-2">
+            <Link to="/add-plane" className="btn">Add</Link>
+          </li>
+        </ul>
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item mr-2">
+            <Link to="/account" className="btn">Account</Link>
+          </li>
+          <li className="nav-item mr-2">
+            <Link to="/orders" className="btn">Orders</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
 }
-
-export default MenuBar;
