f4e8a49b2ecf7d1a6c303652cf65382f4d6c7491
Wedell3 <wedel024@umn.edu>
2020-11-12 13:22:38 -0600

    added routes for each indiviual plane page, imported and added AddPlane component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import MenuBar from './MenuBar';
 import Home from './Home';
-import CommercialPlane from './CommercialPlane'
+import CommercialPlaneView from './CommercialPlaneView';
+import CargoPlaneView from './CargoPlaneView';
+import PrivatePlaneView from './PrivatePlaneView';
+import AddPlane from './AddPlane';
+
+import { useState } from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -8,25 +13,42 @@ import {
 import './App.css';
 
 function App() {
+  const [planes, setPlanes] = useState([]);
+
+  // Get all available planes for mapping to routes
+  const getAvailablePlanes = () => {
+    fetch(`http://localhost:8080/api/planes`)
+      .then(response => response.json())
+      .then(data => setPlanes(data));
+  }
+
   return (
     <div className="App">
-
       <Router>
         <MenuBar />
         <Route exact={true} path="/">
           <Home />
         </Route>
         <Route exact={true} path="/commercial-planes">
-          <CommercialPlane />
+          <CommercialPlaneView />
+        </Route>
+        <Route path="/commercial-planes/:id">
+          {/* Commercial plane componenet*/}
         </Route>
         <Route exact={true} path="/cargo-planes">
-          <p1>Cargo plane</p1>
+          <CargoPlaneView />
+        </Route>
+        <Route path="/cargo-planes/:id">
+          {/* Cargo plane componenet*/}
         </Route>
         <Route exact={true} path="/private-planes">
-          <p1>Private plane</p1>
+          <PrivatePlaneView />
+        </Route>
+        <Route path="/private-planes/:id">
+          {/* Private plane componenet*/}  
         </Route>
         <Route exact={true} path="/add-plane">
-          <p1>Add Plane</p1>
+          <AddPlane />
         </Route>
         <Route exact={true} path="/account">
           <p1>account</p1>
@@ -34,6 +56,10 @@ function App() {
         <Route exact={true} path="/orders">
           <p1>order</p1>
         </Route>
+        { planes.length > 0 && planes.map(plane => (
+            {/* Create Separate component to map all planes to individual routes*/}
+        ))
+        }
       </Router>
     </div>
   );
