b80aba7716caffa20e2ff5781e5c5d81011fb48c
Wedell3 <wedel024@umn.edu>
2020-11-12 13:16:31 -0600

    added component to display errors

---
+++ client/src/Errors.js
@@ -0,0 +1,14 @@
+export default function Errors ( {errors} ) {
+    return (
+        <>
+            {errors.length > 0 && (
+                <div class="alert alert-danger">
+                    <p>The following errors occured</p>
+                    {errors.map((error) => (
+                        <p key={error}>{error}</p>
+                    ))}
+                </div>
+            )}
+        </>
+    );
+}
