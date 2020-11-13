0671cda24741524916b64f86d6558d42bd55ea44
Wedell3 <wedel024@umn.edu>
2020-11-13 14:30:29 -0600

    used fetch call to get plane data when component mounts

--- client/src/PrivatePlaneView.js
+++ client/src/PrivatePlaneView.js
@@ -5,37 +5,25 @@ import PlaneCard from "./PlaneCard";
 export default function CommercialPlane() {
   const [planes, setPlanes] = useState([]);
 
-  const getCommercialPlanes = () => {
-    fetch(`http://localhost:8080/api/private-planes`)
+  const getPrivatePlanes = () => {
+    fetch(`http://localhost:8080/api/planes/private`)
       .then(response => response.json())
       .then(data => setPlanes(data));
   }
 
   //Commented out until controller set up
-  //    useEffect(getCommercialPlanes(), []);
+  useEffect(getPrivatePlanes, []);
 
   return (
     <>
       {planes.length > 0 && planes.map(plane => (
         <div className="row justify-content-md-center my-5">
           <PlaneCard
-            key={planes.planeId}
+            key={plane.planeId}
             plane={plane}
-            pathname="/private-planes/"
           />
         </div>
       ))}
-      <div>
-        <div className="row justify-content-md-center my-5">
-          <PlaneCard pathname="Private" />
-        </div>
-        <div className="row justify-content-md-center my-5">
-          <PlaneCard pathname="Plane" />
-        </div>
-        <div className="row justify-content-md-center my-5">
-          <PlaneCard pathname="Cards" />
-        </div>
-      </div>
     </>
   )
 }
