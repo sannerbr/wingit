f9e85605c156d76a8cd5cefa650610f05f387f68
Wedell3 <wedel024@umn.edu>
2020-11-15 11:17:45 -0600

    added cors to enum controller and added endpoints to security config

--- server/src/main/java/learn/wingit/controllers/EnumController.java
+++ server/src/main/java/learn/wingit/controllers/EnumController.java
@@ -3,11 +3,13 @@ package learn.wingit.controllers;
 import learn.wingit.models.Role;
 import learn.wingit.models.Size;
 import learn.wingit.models.Type;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000", "http://127.0.0.1:8081"})
 @RequestMapping("/api")
 public class EnumController {
 
