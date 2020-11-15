883f174faf586ec69d2197821c4db5f62265da59
Wedell3 <wedel024@umn.edu>
2020-11-15 16:04:58 -0600

    added cors annotation

--- server/src/main/java/learn/wingit/controllers/AuthController.java
+++ server/src/main/java/learn/wingit/controllers/AuthController.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000", "http://127.0.0.1:8081"})
 public class AuthController {
 
 
