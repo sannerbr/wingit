23dac7f178c3fe28fe676ebec8b081c059ba932a
Wedell3 <wedel024@umn.edu>
2020-11-16 09:08:06 -0600

    added conditional rendering for edit and delete buttons for plane card

--- client/src/PlaneCard.js
+++ client/src/PlaneCard.js
@@ -1,8 +1,12 @@
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+
+import AuthContext from './AuthContext'
 
 export default function PlaneCard({ plane, pathname }) {
-  //const planePathname = pathname + plane.planeId;
+  const auth = useContext(AuthContext)
   const planePathname = "/"
+
 
   return (
     <div className="col-sm-10">
@@ -16,20 +20,27 @@ export default function PlaneCard({ plane, pathname }) {
           <div className="col-md-6">
             <div className="card-body">
               <h4 className="card-title">{`${plane.model.manufacturer.name} ${plane.model.name}`}</h4>
-              <br/>
+              <br />
               <div className="row justify-content-center">
                 <p className="card-text mx-4">{`Quantity Available: ${plane.quantity}`}</p>
-                <p className="card-text mx-4">{`Price: ${plane.price}`}</p>
+                <p className="card-text mx-4">{`Price: $${plane.price}`}</p>
               </div>
             </div>
           </div>
           <div className="col-md-3">
             <div className="row justify-content-center">
-              <Link to={planePathname} className="btn btn-secondary my-4">More Info</Link>
+              <Link to={planePathname} className="btn btn-secondary my-3 px-6">More Info</Link>
             </div>
             <div className="row justify-content-center">
-              <button className="btn btn-primary">Add to Order</button>
+              <button className="btn btn-primary mb-3 mx-6">Add to Order</button>
             </div>
+            {
+              auth.user &&
+              <div className="row justify-content-center mb-3">
+                <Link to={planePathname} className="btn btn btn-warning  mr-2">Edit</Link>
+                <button className="btn btn btn-danger ">Delete</button>
+              </div>
+            }
           </div>
         </div>
       </div>
