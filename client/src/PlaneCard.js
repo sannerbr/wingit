c9de260ca3f3c26cbe3e21a2e6fff7983e328709
Wedell3 <wedel024@umn.edu>
2020-11-17 16:47:30 -0600

    conditionally rendered buttons

--- client/src/PlaneCard.js
+++ client/src/PlaneCard.js
@@ -3,7 +3,7 @@ import { useContext } from 'react';
 
 import AuthContext from './AuthContext'
 
-export default function PlaneCard({ plane }) {
+export default function PlaneCard({ plane, rerenderData }) {
   const auth = useContext(AuthContext)
 
   const history = useHistory();
@@ -16,9 +16,10 @@ export default function PlaneCard({ plane }) {
     }
     fetch(`http://localhost:8080/api/planes/id/${plane.plane_id}`, obj)
       .then(response => {
-        if(response.status === 204) {
+        if (response.status === 204) {
           alert("Plane deleted from visible catalog")
-        } else if(response.status === 404) {
+          rerenderData();
+        } else if (response.status === 404) {
           alert("Plane not found")
         } else {
           alert("Unknown Error Occured")
@@ -26,8 +27,14 @@ export default function PlaneCard({ plane }) {
       })
   }
 
+  const makeVisible = () => {
+
+  }
+
   return (
+
     <div className="col-sm-10">
+      {console.log(plane)}
       <div className="card mb-3">
         <div className="row">
           <div className="col-md-3">
@@ -49,15 +56,31 @@ export default function PlaneCard({ plane }) {
             <div className="row justify-content-center">
               <Link to={`planes/${plane.plane_id}`} className="btn btn-secondary my-3 px-6">More Info</Link>
             </div>
-            <div className="row justify-content-center">
-              <button className="btn btn-primary mb-3 mx-6">Add to Order</button>
-            </div>
+            {
+              (!auth.user || (auth.user && auth.user.hasRole("ROLE_USER"))) &&
+              <div className="row justify-content-center">
+                <Link to={`/purchase/${plane.plane_id}`} className="btn btn-lg btn-primary">Purchase</Link>
+              </div>
+            }
+
             {
               auth.user && auth.user.hasRole("ROLE_ADMIN") &&
-              <div className="row justify-content-center mb-3">
-                <Link to={`edit/planes/${plane.plane_id}`} className="btn btn btn-warning mr-2">Edit</Link>
-                <button className="btn btn btn-danger" onClick={handleDelete}>Delete</button>
-              </div>
+              <>
+                <div className="row justify-content-center mb-3">
+                  <Link to={`edit/planes/${plane.plane_id}`} className="btn btn btn-warning mr-2">Edit</Link>
+                  {
+                    !plane.hidden &&
+                    <button className="btn btn btn-danger" onClick={handleDelete}>Delete</button>
+                  }
+                </div>
+
+                <div className="row justify-content-center mb-3">
+                  {
+                    plane.hidden &&
+                    <button className="btn btn btn-danger" onClick={makeVisible}>Make Available</button>
+                  }
+                </div>
+              </>
             }
           </div>
         </div>
