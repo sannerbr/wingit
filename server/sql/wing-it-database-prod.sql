1768e388f5f1c8864229f01549007874ea13a371
Brian Sanner <sanner@dev-10.com>
2020-11-19 11:03:02 -0600

    Add real data to the database

--- server/sql/wing-it-database-prod.sql
+++ server/sql/wing-it-database-prod.sql
@@ -111,7 +111,7 @@ insert into manufacturer(name)
 insert into model(name, manufacturer_id)
     values ('747', 1),
             ('777', 1),
-            ('A220', 2),
+            ('A400M', 2),
             ('C-150', 1),
             ('Learjet 23', 2),
             ('Douglas DC-3', 2);
@@ -128,10 +128,10 @@ insert into `type`(name)
 
 insert into plane(model_id, size_id, type_id, price, quantity, seating_capacity,
                     height, length, wingspan, hidden, `range`, `description`)
-    values (1, 1, 1, 100.00, 1, 100, 10, 10, 10, 0,  100, 'Boeing 747 desc'),
-            (2, 2, 2, 200.00, 2, 200, 20, 20, 20, 0, 200, 'Boeing 777 desc'),
-            (4, 3, 3, 600.00, 2, 400, 40, 40, 40, 1, 400, 'Boeing C-150 desc'),
-            (3, 3, 3, 300.00, 3, 300, 30, 30, 30, 0, 300, 'Airbus A220 desc');
+    values (1, 2, 1, 418000.00, 15, 410, 63, 250, 224, 0,  7730, 'The First Class and Business Class sections of the 747-8 enable airlines to offer passengers the most private and premium accommodations in the sky. So it''s no surprise that on high-volume routes the 747-8 offers premium revenue potential. And with more than 400 seats available, this 747 creates a unique opportunity to maximize the bottom-line potential of any high-volume route. It''s premium value, delivered, for operators and passengers.'),
+            (2, 2, 1, 315000.00, 22, 317, 61, 209, 212, 0, 8555, 'The Boeing 777''s unique combination of superior range, outstanding fuel efficiency and passenger-preferred comfort has created long-range success for carriers around the world. And the 777-300ER now gives operators a perfect opportunity to extend that success. Recent upgrades further reduce costs and boost revenue, and the 777''s flying experience is still number one with passengers. A more profitable fleet and more satisfied passengers.'),
+            (4, 1, 3, 650000.00, 21, 8, 19, 80, 76, 1, 3150, 'The all-new C-150 is purpose-built from the ground up — redesigned to be the toughest, most productive C-150 ever. This is what happens when you merge premium-grade muscle with finely tuned intelligence and design. A beast with brains. A relentlessly tough, high-strength, military-grade, aluminum-alloy body and torture-tested high-strength steel frame with new tech to help you work smarter and harder.'),
+            (3, 3, 2, 360000.00, 15, 25, 48, 148, 139, 0, 4800, 'The A400M "Atlas" is the most advanced, proven and certified airlifter available, combining 21st century state-of-the-art technologies to fulfil the current and upcoming armed forces’ needs. The A400M combines the capability to carry strategic loads with the ability to deliver even into tactical locations with small and unprepared airstrips and can act as a frontline-tanker. One aircraft that can do the work of three – the A400M.');
             
 insert into role (role)
     values ('user'),
@@ -144,9 +144,9 @@ insert into user(role_id, username, password_hash, email, phone, address)
 
 
 insert into `order`(user_id, order_date, total_cost)
-    values (1, '2020-01-01', 100),
-            (1, '2020-02-02', 200),
-            (2, '2020-03-03', 300);
+    values (1, '2020-01-01', 733000),
+            (1, '2020-02-02', 630000),
+            (2, '2020-03-03', 1950000);
 
 insert into order_plane (order_id, plane_id, number_ordered)
     values(1, 1, 1),
